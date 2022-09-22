35659e293ba757b47ee32fe5fc7322edcb2bba3c
Jui Te Chin <pikachin.cs10@nycu.edu.tw>
2022-09-22 21:08:36 +0800

    fix cmake lists bug and update gitignore

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include <gtest/gtest.h>
 
-int main()
-{
-    std::cout << "hello world" << std::endl;
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
 }

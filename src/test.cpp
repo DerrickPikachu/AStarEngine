e06445b11ba8c531095249c29f47da5432663255
Jui Te Chin <pikachin.cs10@nycu.edu.tw>
2022-10-01 18:01:42 +0800

    update cmake to build test with pybind

--- src/test.cpp
+++ src/test.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <Python.h>
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
@@ -6,4 +7,8 @@ TEST(HelloTest, BasicAssertions) {
   EXPECT_STRNE("hello", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(HelloTest, PythonTest) {
+  PyObject * v = PyLong_FromLong(2000000);
 }
